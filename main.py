3512a718def0d8c44c53bab4b40c51e241a34eac
Augusto <august.rosa@hotmail.com>
2025-05-07 08:27:54 -0400

    add entrypoint script to run Azure Blob upload

--- main.py
+++ main.py
@@ -1,41 +1,9 @@
-import io
-import os
-import pyarrow as pa
-import pyarrow.parquet as pq
-from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient, BlobClient
-from owid import catalog
-from datetime import date
+from src import azure_upload # Imports the module responsible for uploading to Azure Blob Storage
 
-# Use a URI to load the dataset below
-rc = catalog.RemoteCatalog()
-uri = "garden/covid/latest/compact/compact" #Consigo chamar um dataset especifico com base na URL
-df = rc[uri]
+# Executes the main upload function
+def main():
 
-# Converts DataFrame to Parquet table
-table = pa.Table.from_pandas(df)
-buffer = io.BytesIO()
-pq.write_table(table, buffer)
-buffer.seek(0)
+    azure_upload.upload_file_to_blob()
 
-# Creating a Client Object
-conection_string = "DefaultEndpointsProtocol=https;AccountName=blobcontainercovid19;AccountKey=yz3VuTcbLoVlYYqNgutA+QqDI+EzHnJhSE6vYgNlrrhgXXUtCfVMN1ZfMTJmUNlElobWhRVBFWnH+ASt7oMZYA==;EndpointSuffix=core.windows.net"
-
-blob_service_client = BlobServiceClient.from_connection_string(conection_string)
-
-container_name = "covid19-raw-data"
-current_file_date = date.today()
-blob_file_Name = f"{current_file_date}/owid-covid-data.parquet"
-
-
-# Client object
-blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_file_Name) #Tentar chamar a data aqui depois
-
-# Upload file
-blob_client.upload_blob(buffer, overwrite=True)
-
-print(f"File sent as {blob_file_Name}")
-
-
-
-
+if __name__ == "__main__":
+    main()
