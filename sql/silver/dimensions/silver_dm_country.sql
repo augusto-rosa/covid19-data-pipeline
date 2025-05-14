a6521634b49dbcf9674810dd0be5597252c4fc56
Augusto <august.rosa@hotmail.com>
2025-05-14 16:21:31 -0400

    update and completion of scripts related to Time and Country dimensions

--- sql/silver/dimensions/silver_dm_country.sql
+++ sql/silver/dimensions/silver_dm_country.sql
@@ -16,7 +16,7 @@ SELECT TOP 10 * FROM bronze.covid19data;
 --
 -- Criando a tabela dm_country
 --
-DROP TABLE IF EXISTS silver.dm_country
+DROP TABLE IF EXISTS silver.dm_country;
 --
 CREATE TABLE silver.dm_country (
 country_id INT,
@@ -30,11 +30,11 @@ PRIMARY KEY (country_id));
 --
 WITH ranked_population AS (
 SELECT
-	code AS country_code
+	iso_code AS country_code
 ,	continent
-,	country
+,	location AS country
 ,	population
-,	ROW_NUMBER() OVER(PARTITION BY code ORDER BY population DESC) AS population_per_country --  maior população dentro de cada país
+,	ROW_NUMBER() OVER(PARTITION BY iso_code ORDER BY population DESC) AS population_per_country --  maior população dentro de cada país
 FROM bronze.covid19data
 )
 INSERT INTO silver.dm_country (country_id, country_code, continent, country, population) -- Inserindo valores na dm_country com base na subquery abaixo
@@ -48,12 +48,65 @@ FROM ranked_population
 WHERE 1=1
 AND population_per_country = 1 -- Irá me trazer o maior valor da população de cada pais
 AND country_code IS NOT NULL
-AND country_code NOT IN ('OWID_WRL', 'OWID_TRS', 'OWID_SAM', 'OWID_OCE', 'OWID_NAM', 'OWID_KOS', 'OWID_EUR', 'OWID_EU27', 'OWID_ASI', 'OWID_AFR')
+AND country_code NOT IN ('OWID_WRL', 'OWID_TRS', 'OWID_SAM', 'OWID_OCE', 'OWID_NAM', 'OWID_KOS', 
+'OWID_EUR', 'OWID_EU27', 'OWID_ASI', 'OWID_AFR', 'OWID_EUN', 'OWID_HIC', 'OWID_LIC', 'OWID_LMC', 'OWID_UMC')
 ORDER BY country ASC;
 --
--- Validando os registros na dm_country
+-- Validando os registros inseridos na dm_country
 --
+SELECT * FROM silver.dm_country;
+--
+-- Alterando o nome de "Micronesia (country)" para "Micronesia"
 SELECT * FROM silver.dm_country
+WHERE 1=1
+AND country_id = 137; -- id do pais 'Micronesia' 
+--
+-- Alterando o nome para "Micronesia"
+UPDATE silver.dm_country
+SET country = 'Micronesia'
+WHERE 1=1 
+AND country_id = 137;
+--
+-- Atualizando o Country_code de England, Northern Ireland, Scotland, Wales e Northern Cyprus
+--
+-- ** England ** 
+--
+UPDATE silver.dm_country
+SET country_code = 'ENG'
+WHERE 1=1 
+AND country_id = 64;
+--
+-- ** Northern Ireland ** 
+--
+UPDATE silver.dm_country
+SET country_code = 'NIR'
+WHERE 1=1 
+AND country_id = 159;
+--
+-- ** Scotland ** 
+--
+UPDATE silver.dm_country
+SET country_code = 'SCT'
+WHERE 1=1 
+AND country_id = 191;
+--
+-- ** Wales ** 
+--
+UPDATE silver.dm_country
+SET country_code = 'WLS'
+WHERE 1=1 
+AND country_id = 237;
+--
+-- ** Northern Cyprus ** 
+--
+UPDATE silver.dm_country
+SET country_code = 'CYN'
+WHERE 1=1 
+AND country_id = 158;
+--
+-- Validando os dados após atualizações
+SELECT * FROM silver.dm_country;
+
 
 
 
--- sql/silver/dimensions/silver_dm_time.sql
+++ sql/silver/dimensions/silver_dm_time.sql
@@ -2,7 +2,7 @@
 
 Author: Augusto Rosa
 
-Source: Silver layer (silver.covid19data_silver)
+Source: Bronze layer (bronze.covid19data)
 
 Description: Create DM_TIME
 
@@ -10,3 +10,55 @@ Language Used: SQL, T-SQL
 
 */
 --
+USE azure_projects
+GO
+SELECT TOP 10 * FROM bronze.covid19data;
+--
+-- Deletando a tabela caso ela já exista
+--
+DROP TABLE IF EXISTS silver.dm_time;
+--
+-- Criando a tabela silver.dm_time
+--
+CREATE TABLE silver.dm_time (
+date_integer_id INT,
+[date] DATE,
+[year] INT,
+[quarter] TINYINT,
+[month] TINYINT,
+[week] TINYINT,
+[day] TINYINT,
+[weekday] TINYINT,
+PRIMARY KEY (date_integer_id));
+--
+-- Gerando os valores a serem inseridos na dm_country
+--
+WITH distinct_date AS (
+SELECT 
+	[date] AS unique_date
+,	ROW_NUMBER() OVER(PARTITION BY [date] ORDER BY [date] ASC) AS unique_date_rn
+FROM bronze.covid19data
+) INSERT INTO silver.dm_time (date_integer_id, [date], [year], [quarter], [month], [week], [day], [weekday])
+SELECT
+-- Gera o date_id no formato YYYYMMDD como INT para uso como chave estrangeira (FK) nas tabelas fato,
+-- proporcionando melhor performance em joins e menor consumo de espaço comparado com tipos DATE/VARCHAR
+	CAST(REPLACE(unique_date, '-', '') AS INT) AS date_integer_id
+,	CAST(unique_date AS DATE) AS [date]
+,	DATEPART(YEAR, unique_date) AS [year]
+,	DATEPART(QUARTER, unique_date) AS [quarter]
+,	DATEPART(MONTH, unique_date) AS [month]
+,	DATEPART(WEEK, unique_date) AS [week]
+,	DATEPART(DAY, unique_date) AS [day]
+,	DATEPART(WEEKDAY, unique_date) AS [weekday]
+FROM distinct_date
+WHERE 1=1
+AND unique_date_rn = 1
+ORDER BY unique_date ASC
+--
+-- Validando os registros inseridos na silver.dm_time
+--
+SELECT 
+    COUNT(*) AS total_records,
+    MIN([date]) AS min_date,
+    MAX([date]) AS max_date
+FROM silver.dm_time;
