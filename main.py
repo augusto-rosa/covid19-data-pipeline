857cff1a3015041ad346353b8f64802c84ab972d
Augusto <august.rosa@hotmail.com>
2025-05-28 14:39:39 -0400

    Adjust function call in main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from src import azure_upload # Imports the module responsible for uploading to Azure Blob Storage
+from src import azure_upload # Imports the module responsible for uploading to Azure Data Lake Gen2
 
 # Executes the main upload function
 def main():
 
-    azure_upload.upload_file_to_blob()
+    azure_upload.upload_file_to_data_lake()
 
 if __name__ == "__main__":
     main()
